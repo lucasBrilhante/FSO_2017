bfb7523ccac85a0db2560e9a9c6b216a99e3728d
Lucas Brilhante <lbrilhante82@gmail.com>
2017-03-16 23:05:12 -0300

    Adicionando estrutura de ifs

--- exercicio1.c
+++ exercicio1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 typedef struct{
 	float x;
@@ -12,14 +13,22 @@ typedef struct {
 	coordenada l; 
    } quadrado;
  
+float eConvexo(quadrado quad){}
+
 int main(){
 	quadrado q;
 	scanf("%f%f",&q.i.x, &q.i.y);
 	scanf("%f%f",&q.j.x, &q.j.y);
 	scanf("%f%f",&q.k.x, &q.k.y);
 	scanf("%f%f",&q.l.x, &q.l.y);
+
+	if(eConvexo()){
+		printf("É convexo\n");
+	}else{
+		printf("Não é convexo\n");
+	}
+
 	printf("%f",q.i.x+q.i.y+q.j.x+q.j.y+q.k.x+q.k.y+q.l.x+q.l.y);
 	
 	return 0;
-
 }
