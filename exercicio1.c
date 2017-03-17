934dc4461730f1cbd502a1fbf0117f7171caf7b1
Lucas Brilhante <lbrilhante82@gmail.com>
2017-03-16 23:54:31 -0300

    Tentando utilizar formulas com pontos. Precisa transformar ponto pra vetor

--- exercicio1.c
+++ exercicio1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+#define PI 3.14159265
+
 typedef struct{
 	float x;
 	float y;
@@ -12,8 +14,22 @@ typedef struct {
 	coordenada k;
 	coordenada l; 
    } quadrado;
- 
-float eConvexo(quadrado quad){}
+
+double pitagoras(double x, double y){
+	return sqrt((x*x) + (y*y));
+} 
+
+int eConvexo(quadrado quad){
+	/*double fatorDeConversao = 180.0 / PI;
+	double resultado = 0;
+	double temp1 = 0;
+	double temp2 = 0;
+	temp1 = quad.i.x*quad.j.x + quad.i.y*quad.j.y;
+	temp2 = pitagoras(quad.i.x,quad.j.x)*pitagoras(quad.i.y, quad.j.y);
+	resultado = acos(temp1/temp2) * fatorDeConversao;
+	printf("%lf\n",resultado);*/
+	return 1;
+}
 
 int main(){
 	quadrado q;
@@ -22,13 +38,11 @@ int main(){
 	scanf("%f%f",&q.k.x, &q.k.y);
 	scanf("%f%f",&q.l.x, &q.l.y);
 
-	if(eConvexo()){
+	if(eConvexo(q) == 1){
 		printf("É convexo\n");
 	}else{
 		printf("Não é convexo\n");
 	}
-
-	printf("%f",q.i.x+q.i.y+q.j.x+q.j.y+q.k.x+q.k.y+q.l.x+q.l.y);
 	
 	return 0;
 }
