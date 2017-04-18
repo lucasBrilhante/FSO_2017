377724fcba00964a3cf0c3fdc3b488db7da72d2e
Lucas Brilhante <lbrilhante82@gmail.com>
2017-04-17 23:39:42 -0300

    Criando processos e timers

--- Trabalho2/fork.c
+++ Trabalho2/fork.c
@@ -5,7 +5,7 @@
 
 int main(int argv, char *argc[])
 {
-    clock_t begin = clock();
+    
     int i;
     pid_t childpid;
     childpid = fork();
@@ -14,12 +14,22 @@ int main(int argv, char *argc[])
         perror("Failed to Fork");
         return 1;
     }
-    for(i=0; i<3; i++)
-    {
-        if(childpid == 0)
-            printf("You are in Child: %ld\n", (long)getpid());
-        else
-            printf("You are in Parent: %ld\n", (long)getpid());
+    clock_t begin = clock();
+            
+    for(i=0;i<500000;i++){
+        printf("a");
     }
+
+    if(childpid == 0){
+            clock_t end = clock();
+            double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+            printf("pai: %lf\n", time_spent);
+        }    
+        else{
+            clock_t end1 = clock();
+            double time_spent1 = (double)(end1 - begin) / CLOCKS_PER_SEC;
+            printf("filho: %lf\n", time_spent1);
+        }
+
     return 0;
 }
