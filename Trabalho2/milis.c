947e239ed9dc1402b24957dcc6b7f369746d8da2
Lucas Brilhante <lbrilhante82@gmail.com>
2017-04-19 19:41:15 -0300

    funcionando select()

--- Trabalho2/milis.c
+++ Trabalho2/milis.c
@@ -27,78 +27,100 @@ int tempoFinal(struct timeval inicio){
     
     return segundos;    
 }
-/*char * getline(void) {
-    char * line = malloc(100), * linep = line;
-    size_t lenmax = 100, len = lenmax;
-    int c;
 
-    if(line == NULL)
-        return NULL;
+int get_largest_file_descriptor(int pipe_child1[], int pipe_child2[]){
+  int largest;
 
-    for(;;) {
-        c = fgetc(stdin);
-        if(c == EOF)
-            break;
+  largest = pipe_child1[0];
 
-        if(--len == 0) {
-            len = lenmax;
-            char * linen = realloc(linep, lenmax *= 2);
+  if (pipe_child1[1] > largest)
+    largest = pipe_child1[1];
+  if (pipe_child2[0] > largest)
+    largest = pipe_child2[0];
+  if (pipe_child2[1] > largest)
+    largest = pipe_child2[1];
 
-            if(linen == NULL) {
-                free(linep);
-                return NULL;
-            }
-            line = linen + (line - linep);
-            linep = linen;
-        }
+  return largest;
+}
 
-        if((*line++ = c) == '\n')
-            break;
-    }
-    *line = '\0';
-    return linep;
-}*/
 void processoPai(struct timeval inicio){
-
+    fd_set read_set;
+    struct timeval tv;
+    int retval;
     char str_recebida[BUFFER];
+    char str_recebida2[BUFFER];
+    
     FILE *f = fopen("output.txt", "w");
+
     if (f == NULL)
     {
         printf("Error opening file!\n");
         exit(1);
     }
     while(tempoFinal(inicio) < 30){
-        /* No filho, vamos ler. Então vamos fechar a entrada de ESCRITA do pipe */
         close(pipe_child1[1]);
-     
-        /* Lendo o que foi escrito no pipe, e armazenando isso em 'str_recebida' */
-        read(pipe_child1[0], str_recebida, sizeof(str_recebida));
-        if(strcmp(str_recebida, "")){
-            //printf("String enviada pelo filho no Pipe : '%s'\n", str_recebida);
-            fprintf(f, "%s\n", str_recebida);
-        }
-
         close(pipe_child2[1]);
+
+        FD_ZERO(&read_set);
+        FD_SET(pipe_child1[0], &read_set);
+        FD_SET(pipe_child2[0], &read_set);
+        tv.tv_sec = 6;  
+        tv.tv_usec = 0;
+        retval = select(get_largest_file_descriptor(pipe_child1,pipe_child2)+1, &read_set, NULL, NULL, &tv);
+
+        
+        if(retval == -1)
+            printf("Select failed.\n");
+        else if(retval)
+        {
+           printf("Exite pelo menos um pipe ready %d.\n",retval);
+
+            
+            if (FD_ISSET(pipe_child1[0], &read_set)){
+                /* Lendo o que foi escrito no pipe, e armazenando isso em 'str_recebida' */
+                
+     
+                /* Lendo o que foi escrito no pipe, e armazenando isso em 'str_recebida' */
+                read(pipe_child1[0], str_recebida, sizeof(str_recebida));
+                //if(strcmp(str_recebida, "")){
+                printf("String enviada pelo filho preg no Pipe : '%s'\n", str_recebida);
+                fprintf(f, "%s\n", str_recebida);
+                //}
+            }
+            if (FD_ISSET(pipe_child2[0], &read_set)){
+                /* Lendo o que foi escrito no pipe, e armazenando isso em 'str_recebida' */
+                read(pipe_child2[0], str_recebida2, sizeof(str_recebida2));
+                //if(strcmp(str_recebida2, "")){
+                printf("String enviada pelo filho ativo no Pipe : '%s'\n", str_recebida2);
+                fprintf(f, "%s\n", str_recebida2);
+                //}
+            }           
+        }
+        else printf("No data written to pipe in 6 last seconds.\n");
+
+
+
+        //select(2, &fds, NULL, NULL, NULL);
+        /* No filho, vamos ler. Então vamos fechar a entrada de ESCRITA do pipe */
+        
+        
+        
      
         /* Lendo o que foi escrito no pipe, e armazenando isso em 'str_recebida' */
-        read(pipe_child2[0], str_recebida, sizeof(str_recebida));
-        if(strcmp(str_recebida, "")){
-            //printf("String enviada pelo filho no Pipe : '%s'\n", str_recebida);
-            fprintf(f, "%s\n", str_recebida);
-        }
+        
         /* print some text */
         //const char *text = "Write this to the file";
         
     }
     fclose(f);
 }
-void processoPreg(){
-    struct timeval inicio, final;
+void processoPreg(struct timeval inicio){
+    struct timeval final;
     int count = 0;
     while(*glob_var == 0){
         count++;
         //começa o tempo
-        gettimeofday(&inicio, NULL);
+        //gettimeofday(&inicio, NULL);
         //Escrever no pipe
         
         //exit(0);
@@ -111,7 +133,6 @@ void processoPreg(){
         gettimeofday(&final, NULL);
 
         /* --------Formatando tempo-----------*/
-
         int segundos = (int) final.tv_sec - inicio.tv_sec;
         int milisegundosF = (int) final.tv_usec;
         int milisegundosI = (int) inicio.tv_usec;
@@ -131,8 +152,8 @@ void processoPreg(){
         //printf("filho preguiçoso: %d:%d:%d\n",minutos,segundos%60,milisegundos); 
     }
 }
-void processoAtivo(){
-    struct timeval inicio, final;
+void processoAtivo(struct timeval inicio){
+    struct timeval final;
     int count = 0;
     while(*glob_var == 0){
         count++;
@@ -143,8 +164,12 @@ void processoAtivo(){
         //exit(0);
         //char* buff = getline();
         char buff[BUFFER];
-
+        SleepFor(5);
+        //buff[0] = ' ';
+        //printf("Insira a mensagem a qual deseja enviar.\n");
+        fflush(stdin);
         scanf("%s", buff);
+        getchar();
         //finaliza contagem
         gettimeofday(&final, NULL);
 
@@ -162,7 +187,7 @@ void processoAtivo(){
 
         char str[BUFFER];
         sprintf(str,"%d:%d:%d:  Mensagem %d do usuario: <%s>",minutos,segundos%60,milisegundos,count,buff);
-        printf("String enviada para pai no Pipe: '%s'\n", str);
+        //printf("String enviada para pai no Pipe: '%s'\n", str);
 
         /* Escrevendo a string no pipe */
         write(pipe_child2[1], str, sizeof(str) + 1);
@@ -186,7 +211,7 @@ int main() {
         return -1 ;
     }
    
-    if(pipe(pipe_child1) < 0) {
+    if(pipe(pipe_child2) < 0) {
         perror("Error creating pipe for child2") ;
         return -1 ;
     }
@@ -207,7 +232,7 @@ int main() {
     
     if(filhoP != 0){
         printf("filho preguiçoso id: %d\n",filhoP); 
-        processoPreg();
+        processoPreg(inicio);
         return 0;
     }
     //criando processo Ativo
@@ -216,7 +241,7 @@ int main() {
     
     if(filhoA != 0){
         printf("filho ativo id: %d\n",filhoA); 
-        processoAtivo();
+        processoAtivo(inicio);
         return 0;
     }
 
